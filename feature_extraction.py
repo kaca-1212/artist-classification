8df1c528019606ce09bc28cc456e8b91662b0825
Andrew Deng <dsyandrewdeng@gmail.com>
2018-12-01 08:55:36 -0800

    updates to svm part

--- feature_extraction.py
+++ feature_extraction.py
@@ -5,12 +5,15 @@
 # organize imports
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import MinMaxScaler
+from skimage.feature import greycomatrix, greycoprops
+from sklearn.cluster import MiniBatchKMeans
 import numpy as np
 import mahotas
 import cv2
 import os
 import h5py
 import sys
+import pickle
 # fixed-sizes for image
 fixed_size = tuple((256, 256))
 
@@ -25,6 +28,39 @@ test_size = 0.10
 
 # seed for reproducing same results
 seed = 9
+
+# BOVW for SIFT
+def create_BOVW(path):
+    try:
+        kmeans = pickle.load(open('bovw_dict.sav', 'rb'))
+    except:
+        sift = cv2.xfeatures2d.SIFT_create(nfeatures=50)
+        descriptors =np.array(0)
+        for file in os.listdir(path):
+            print file
+            try:
+                image = cv2.imread(path+'/'+file)
+            except:
+                continue
+            if image is None:
+                continue
+            image = cv2.resize(image,fixed_size)
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            kp, desc = sift.detectAndCompute(gray,None)
+            if len(np.shape(descriptors)) == 0:
+                descriptors = np.array(desc)
+            else:
+                descriptors = np.row_stack((descriptors, np.array(desc)))
+            print np.shape(descriptors)
+        batch_size = np.shape(descriptors)[0]/10
+        kmeans = MiniBatchKMeans(n_clusters=300,batch_size=batch_size)
+        kmeans.fit(np.array(descriptors))
+        pickle.dump(kmeans, open('bovw_dict.sav','wb'))
+
+    return kmeans
+
+
+
 
 # feature-descriptor-1: Hu Moments
 def fd_hu_moments(image):
@@ -52,11 +88,16 @@ def fd_histogram(image, mask=None):
     # return the histogram
     return hist.flatten()
 
-def fd_sift(image):
+
+def fd_sift(image, bovw_dict):
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    sift = cv2.SIFT()
-    kp = sift.detect(gray,None)
-    return kp
+    sift = cv2.xfeatures2d.SIFT_create()
+    kp, desc = sift.detectAndCompute(gray,None)
+    hist = np.zeros([300,1])
+    for d in desc:
+        cluster = bovw_dict.predict(d)
+        hist[cluster] += 1
+    return list(hist)
 
 def fd_hog(image):
     hog = cv2.HOGDescriptor()
@@ -65,6 +106,13 @@ def fd_hog(image):
     h = hog.compute(image, locations = locations)
     return h.flatten()
 
+def fd_orb(image):
+    orb = cv2.ORB()
+    kp = orb.detect(image, None)
+    kp, des = orb.compute(img, kp)
+    return kp
+
+active_fd = [0,0,0,1,0,0]
 # # get the training labels
 # train_labels = os.listdir(train_path)
 
@@ -79,7 +127,7 @@ def get_feature_vectors(path):
     missed_files = []
     # # num of images per class
     # images_per_class = 80
-    
+    bovw_dict = create_BOVW(path)
     checkpoint = 1
     
     # loop over the training data sub-folders
@@ -116,14 +164,25 @@ def get_feature_vectors(path):
         # Global Feature extraction
         ####################################
         #fv_hog = fd_hog(image)
-        fv_haralick   = fd_haralick(image)
-        fv_histogram  = fd_histogram(image)
-        fv_hu = fd_hu_moments(image)
+        feature_list = []
+        if active_fd[0]:
+            feature_list.append(fd_hu_moments(image))
+        if active_fd[1]:
+            feature_list.append(fd_haralick(image))
+        if active_fd[2]:
+            feature_list.append(fd_histogram(image))
+        if active_fd[3]:
+            feature_list.append(fd_sift(image, bovw_dict))
+        if active_fd[4]:
+            feature_list.append(fd_hog(image))
+        if active_fd[5]:
+            feature_list.append(fd_orb(image))
+        
 
         ###################################
         # Concatenate global features
         ###################################
-        global_feature = np.hstack([fv_histogram, fv_haralick, fv_hu])
+        global_feature = np.hstack(feature_list)
 
         # update the list of labels and feature vectors
         labels.append(current_label)
@@ -168,11 +227,14 @@ def get_feature_vectors(path):
     print "[STATUS] target labels: {}".format(target)
     print "[STATUS] target labels shape: {}".format(target.shape)
     
+
+    encoding = int("".join(map(str, active_fd)), base=2)
+
     # save the feature vector using HDF5
-    h5f_data = h5py.File('output/data3.h5', 'w')
+    h5f_data = h5py.File('output/data_' + str(encoding) + '.h5', 'w')
     h5f_data.create_dataset('dataset_1', data=np.array(global_features))
 
-    h5f_label = h5py.File('output/labels3.h5', 'w')
+    h5f_label = h5py.File('output/labels_' + str(encoding) + '.h5', 'w')
     h5f_label.create_dataset('dataset_1', data=np.array(target))
 
     h5f_data.close()
--- train_test.py
+++ train_test.py
@@ -15,11 +15,11 @@ import glob
 import cv2
 from matplotlib import pyplot
 from sklearn.model_selection import train_test_split, cross_val_score
-from sklearn.model_selection import KFold, StratifiedKFold
+from sklearn.model_selection import KFold, StratifiedKFold, StratifiedShuffleSplit
 from sklearn.model_selection import GridSearchCV
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
-from sklearn.svm import LinearSVC
+from sklearn.svm import SVC
 from sklearn.externals import joblib
 from feature_extraction import get_feature_vectors
 
@@ -27,8 +27,11 @@ from feature_extraction import get_feature_vectors
 test_size = 0.10
 
 # create all the machine learning models
-#model = SVC(kernel='rbf', C = 100.0, gamma='auto')
-model = LogisticRegression(C=10)
+model = SVC(kernel='rbf', C = 200, gamma='auto', decision_function_shape='ovr')
+#param_grid = [ {'C':[0.01,0.1,1,10,100,1000], 'gamma':['auto'],'kernel':['rbf']} ]
+#model = GridSearchCV(SVC(),param_grid = param_grid, cv=StratifiedShuffleSplit(n_splits=5, test_size=0.2, random_state=42))
+#model = LogisticRegression(C=10)
+#model = LinearSVC(C=10, decision_function_shape='ovr')
 
 # variables to hold the results and names
 results = []
@@ -38,27 +41,26 @@ scoring = "accuracy"
 h5f_data = h5py.File('output/data3.h5', 'r')
 h5f_label = h5py.File('output/labels3.h5', 'r')
 
-global_features_string = h5f_data['dataset_1']
+all_features_string = h5f_data['dataset_1']
 all_labels_string = h5f_label['dataset_1']
 
-global_features = np.array(global_features_string)
-global_labels = np.array(all_labels_string)
-
+all_features = np.array(all_features_string)
+all_labels = np.array(all_labels_string)
 
 # use this if feature memory is too large to cast to np.array directly
-"""
+
 
 global_features = np.empty((0,len(h5f_data['dataset_1'][0])), dtype=np.float16)
 global_labels = np.empty((0,1), dtype=int)
 
-sample_proportion = 0.5
+sample_proportion = 1
 print(np.unique(all_labels))
-for label in np.unique(all_labels)[0:10]:
+for label in np.unique(all_labels)[0:5]:
     class_coords = np.where(all_labels == label)[0]
     samples = np.random.choice(class_coords, int(len(class_coords)*sample_proportion), replace =False)
     global_features = np.row_stack((global_features, np.array(h5f_data['dataset_1'])[samples]))
     global_labels = np.append(global_labels, all_labels[samples])
-    """
+    
 h5f_data.close()
 h5f_label.close()
 
@@ -86,14 +88,14 @@ import warnings
 warnings.filterwarnings('ignore')
 
 # 10-fold cross validation
-kfold = KFold(n_splits=10)
-cv_results = cross_val_score(model, trainDataGlobal, trainLabelsGlobal, cv=kfold, scoring=scoring)
-results.append(cv_results)
-msg = "SVM: %f (%f)" % (cv_results.mean(), cv_results.std())
-print msg
+# kfold = KFold(n_splits=10)
+# cv_results = cross_val_score(model, trainDataGlobal, trainLabelsGlobal, cv=kfold, scoring=scoring)
+# results.append(cv_results)
+# msg = "SVM: %f (%f)" % (cv_results.mean(), cv_results.std())
+# print msg
 
 model.fit(trainDataGlobal, trainLabelsGlobal)
-
+print model.cv_results_
 y_pred = model.predict(testDataGlobal)
 print confusion_matrix(testLabelsGlobal, y_pred)
 # path to test data
